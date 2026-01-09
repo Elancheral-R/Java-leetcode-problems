2fd35d8e055bad8f92bd2786c398e0c474741ed6
Elancheral-R <elanelancheral@gmail.com>
2026-01-09 18:55:23 +0530

    Time: 1 ms (59.00%) | Memory: 48.7 MB (24.76%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) 
+    {
+            int arr1[]=new int[height.length];
+            int arr2[]=new int[height.length];
+            int max=-1;
+            for(int i=0;i<height.length;i++)
+            {
+                max=Math.max(max,height[i]);
+                arr1[i]=max;
+            }
+            max=-1;
+            for(int i=height.length-1;i>=0;i--)
+            {
+                max=Math.max(max,height[i]);
+                arr2[i]=max;
+            }
+            int sum=0;
+            for(int i=0;i<height.length;i++)
+            {
+                sum +=(Math.min(arr1[i],arr2[i])-height[i]);
+            }
+            return sum;
+    }
+}
