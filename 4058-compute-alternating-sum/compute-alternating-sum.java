c83769552278a7693cef0200e3d089bcc29bd94c
Elancheral-R <elanelancheral@gmail.com>
2026-05-25 19:39:20 +0530

    Time: 1 ms (98.97%) | Memory: 46.5 MB (24.61%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int alternatingSum(int[] nums) 
+    {
+        boolean toggle=true;
+        int sum=0;
+        for(int number:nums)
+        {
+            if(toggle)
+                {
+                    sum+=number;
+                }
+            else
+            {
+                sum-=number;
+            }
+            toggle=(!toggle);
+        }
+        return sum;
+    }
+}
