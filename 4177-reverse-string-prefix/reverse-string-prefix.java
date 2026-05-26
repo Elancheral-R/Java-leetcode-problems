9fad9a9d037e40404e1da839ccea8ff6d5d85ba4
Elancheral-R <elanelancheral@gmail.com>
2026-05-26 14:43:30 +0530

    Time: 5 ms (15.87%) | Memory: 45.8 MB (12.49%) - LeetSync

---
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reversePrefix(String s, int k) 
+    {
+        if (k==1)
+            return s;
+        String rep = s.substring(0,k);
+        String res="";
+        for(int i=rep.length()-1;i>=0;i--)
+            res+=rep.charAt(i);
+        res+=s.substring(k,s.length());
+        return res;  
+    }
+}
