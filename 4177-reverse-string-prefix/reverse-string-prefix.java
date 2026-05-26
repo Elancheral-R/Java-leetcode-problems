7293996757d9d5b3b022a389fca2de8dc5b78013
Elancheral-R <elanelancheral@gmail.com>
2026-05-26 14:46:30 +0530

    Time: 5 ms (15.87%) | Memory: 45.9 MB (12.49%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.java
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -3,11 +3,16 @@ class Solution {
     {
         if (k==1)
             return s;
-        String rep = s.substring(0,k);
+        // String rep = s.substring(0,k);
+        // String res="";
+        // for(int i=rep.length()-1;i>=0;i--)
+        //     res+=rep.charAt(i);
+        // res+=s.substring(k,s.length());
+        // return res;
         String res="";
-        for(int i=rep.length()-1;i>=0;i--)
-            res+=rep.charAt(i);
+        for(int i=k-1;i>=0;i--)
+            res+=s.charAt(i);
         res+=s.substring(k,s.length());
-        return res;  
+        return res;
     }
 }
