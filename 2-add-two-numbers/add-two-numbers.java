4b51cdf09075a1ee51b49877bbe554b62a6f43e2
Elancheral-R <elanelancheral@gmail.com>
2026-08-10 20:09:09 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (58.26%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,35 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode result = new ListNode();
+        ListNode mov=result;
+        int carry=0;
+        int sum=0;
+        while(l1!=null && l2!=null)
+        {
+            sum=carry+l1.val+l2.val;
+            carry=sum/10;
+            sum=sum%10;
+            mov.next= new ListNode(sum);
+            mov=mov.next;
+            l1=l1.next;
+            l2=l2.next;
 
-        ListNode dummy = new ListNode();
-        ListNode current = dummy;
-
-        int carry = 0;
-
-        while (l1 != null || l2 != null) {
-
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-
-            int sum = x + y + carry;
-
-            carry = sum / 10;
-            int digit = sum % 10;
-
-            current.next = new ListNode(digit);
-            current = current.next;
-
-            if (l1 != null)
-                l1 = l1.next;
-
-            if (l2 != null)
-                l2 = l2.next;
+        }    
+        while(l1!=null)
+        {
+            sum=carry+l1.val;
+            carry=sum/10;
+            sum=sum%10;
+            mov.next= new ListNode(sum);
+            mov=mov.next;
+            l1=l1.next;
         }
-
-        if (carry > 0) {
-            current.next = new ListNode(carry);
+        while(l2!=null)
+        {
+            sum=carry+l2.val;
+            carry=sum/10;
+            sum=sum%10;
+            mov.next= new ListNode(sum);
+            mov=mov.next;
+            l2=l2.next;
         }
-
-        return dummy.next;
+        if(carry==1)
+            mov.next=new ListNode(1);
+            
+        return result.next;
     }
 }
