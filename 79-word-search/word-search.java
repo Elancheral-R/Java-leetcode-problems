2bd33a710cdf125cbd9a675958cf420d6fa5909d
Elancheral-R <elanelancheral@gmail.com>
2026-08-13 14:07:58 +0530

    Time: 150 ms (35.89%) | Memory: 43.1 MB (43.87%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,58 +1,42 @@
-class Solution {
-
+public class Solution {
     public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
 
-        int rows = board.length;
-        int cols = board[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
-
-                    if (dfs(board, word, i, j, 0)) {
-                        return true;
-                    }
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
                 }
             }
         }
-
+        
         return false;
     }
-
-    private boolean dfs(char[][] board, String word,
-                        int row, int col, int index) {
-
-        // We found the complete word
+    
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
         if (index == word.length()) {
             return true;
         }
-
-        // Out of bounds
-        if (row < 0 || row >= board.length ||
-            col < 0 || col >= board[0].length) {
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
-
-        // Current character doesn't match
-        if (board[row][col] != word.charAt(index)) {
-            return false;
+        
+        visited[i][j] = true;
+        
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
         }
-
-        // Mark this cell as visited
-        char temp = board[row][col];
-        board[row][col] = '#';
-
-        // Search in 4 directions
-        boolean found =
-                dfs(board, word, row - 1, col, index + 1) || // up
-                dfs(board, word, row + 1, col, index + 1) || // down
-                dfs(board, word, row, col - 1, index + 1) || // left
-                dfs(board, word, row, col + 1, index + 1);   // right
-
-        // Backtrack: restore original character
-        board[row][col] = temp;
-
-        return found;
+        
+        visited[i][j] = false;
+        return false;
     }
 }
