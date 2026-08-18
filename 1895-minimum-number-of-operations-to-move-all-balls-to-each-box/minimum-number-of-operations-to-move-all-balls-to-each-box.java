0171d758949fcd078d68ea2149a5b36ec2bb0314
Elancheral-R <elanelancheral@gmail.com>
2026-08-18 14:01:11 +0530

    Time: 148 ms (8.10%) | Memory: 46.6 MB (34.12%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -4,13 +4,14 @@ class Solution {
         int res[]= new int[boxes.length()];
         for(int i=0;i<boxes.length();i++)
         {
-            if(boxes.charAt(i) != '1')
-                continue;
             for(int j=0;j<boxes.length();j++)
             {
-                res[j]+=Math.abs(i-j);
+                if(i==j || boxes.charAt(j)=='0')
+                    continue;
+                else
+                res[i]+=Math.abs(i-j);
             }
-        }    
+        }
         return res;
     }
 }
